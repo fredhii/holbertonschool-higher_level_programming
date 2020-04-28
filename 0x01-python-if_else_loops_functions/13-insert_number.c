89663e14a45b633a4ec3231f67de1bdd49e1bfd0
fredhii <fredhiixd@gmail.com>
2020-04-28 12:01:02 -0500

    [Add]: 0x01 mandatory tasks

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - Insert a new node.
+ * @head: Linked list
+ * @number: Number to insert
+ * Return: node address if Success, NULL if Fails.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp, *new;
+
+        tmp = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+
+	/* Check in first position */
+	if (*head == NULL || (*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	/* Check inside list */
+	while (tmp->next)
+	{
+		if (tmp->n >= number)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
+	}
+	/* Insert in last position */
+	tmp->next = new;
+	new->next = NULL;
+	return (new);
+}
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        xd = ord(str[i])
+        if xd >= 97 and xd <= 123:
+            xd -= 32
+        print(chr(xd), end="")
+    print()
