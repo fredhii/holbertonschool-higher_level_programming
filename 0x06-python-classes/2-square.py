58d8ca27958f7b1d05c24c970207f05ae425b34c
fredhii <fredhiixd@gmail.com>
2020-05-19 17:16:44 -0500

    [Fix]: Script documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Square class """
+
+
 class Square:
+    """ Define square size """
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Square class """
+
+
 class Square:
+    """ Define square size """
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -7,6 +11,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    """ Define square area """
     def area(self):
         res = self.__size * self.__size
         return res
