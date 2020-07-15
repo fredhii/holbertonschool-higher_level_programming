fe865d871c249960fcfd7fd12f0f549dc9be15b5
fredhii <fredhiixd@gmail.com>
2020-07-15 03:17:14 +0000

    fix queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- shows grants for two users
+-- Display privileges
+-- Show privileges for two users
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List shows linked to database
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-       FROM tv_genres JOIN tv_show_genres
-       WHERE tv_show_genres.genre_id = tv_genres.id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+       FROM tv_show_genres
+       JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
        GROUP BY tv_show_genres.genre_id
-       ORDER BY number_shows DESC;
+       ORDER BY number_of_shows DESC, tv_genres.id ASC;
