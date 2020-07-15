6c5b323ffd489d7b603b03b480302797c5ba8d65
fredhii <fredhiixd@gmail.com>
2020-07-15 03:08:10 +0000

    fix queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,6 @@
 -- List shows linked to database
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_show_genres
-INNER JOIN  tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name ORDER BY number_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+FROM tv_show_genres
+WHERE tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_shows DESC;
