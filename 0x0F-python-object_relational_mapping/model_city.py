9ae374093702c56e2d1e166d8ea3f9d162af2f69
Fredy Acuna <fredhiixd@gmail.com>
2020-08-08 09:19:03 -0500

    fix error

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,7 @@ contains the class definition of a City
 '''
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from model_state import Base, State
 
 
 class City(Base):
