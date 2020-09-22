bbc9bdb64072f88afd6d624dae176ff25e480f3c
fredhii <fredhiixd@gmail.com>
2020-09-22 01:13:42 +0000

    fix code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-var newDict = {};
+const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
