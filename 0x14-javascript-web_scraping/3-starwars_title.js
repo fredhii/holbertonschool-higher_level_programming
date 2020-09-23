9b44fad662a8bfe4c8066a9aff670095979373d3
fredhii <fredhiixd@gmail.com>
2020-09-22 19:25:55 -0500

    fix url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-let request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episodeNumber = process.argv[2];
-request(url + episodeNumber, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
-  } else {
-    console.log('Wrong status code');
-  }
+const request = require('request');
+
+const url = 'http://swapi.co.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
